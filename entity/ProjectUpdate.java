9aa1a6c8cb2c3040c28490e7e48ae4adfce9482d
yumyum7 <106042368+yumyum7@users.noreply.github.com>
2022-09-05 18:12:08 +0900

    Add files via upload

---
+++ entity/ProjectUpdate.java
@@ -0,0 +1,43 @@
+package com.cat.project;
+
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.cat.account.Account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ProjectUpdate {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long uId;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "project", referencedColumnName = "pId")
+	private Project project;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "account", referencedColumnName = "aId")
+	private Account account;
+	
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String uText;
+	
+	@CreatedDate
+	private LocalDate uTs;
+}
