cd58be809fb456d86b954ddf6f7848bcb41148a6
plasticver2 <108119032+plasticver2@users.noreply.github.com>
2022-09-07 10:17:49 +0900

    Add files via upload

---
+++ src/main/java/com/cat/project/ProjectService.java
@@ -0,0 +1,47 @@
+package com.cat.project;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.cat.DataNotFoundException;
+import com.cat.project.entity.Project;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ProjectService {
+	
+	private final ProjectRepository projectRepository;
+
+    public List<Project> getList() {
+        return this.projectRepository.findAll();
+    }
+    
+    public Project getProject(Long pId) {
+    	Optional<Project> project = this.projectRepository.findById(pId);
+    	if(project.isPresent()) {
+    		return project.get();
+    	}else {
+    		throw new DataNotFoundException("project not found");
+    	}
+    }
+    
+    public void create(String pName, String pCate, String pDesc, BigDecimal pGoal, LocalDate pSdate, LocalDate pEdate, String pCreator) {
+    	Project p = new Project();
+    	p.setPName(pName);
+    	p.setPCate(pCate);
+    	p.setPDesc(pDesc);
+    	p.setPGoal(pGoal);
+    	p.setPSdate(pSdate);
+    	p.setPEdate(pEdate);
+    	p.setPCreator(pCreator);
+    	this.projectRepository.save(p);
+    	
+    }
+
+}
