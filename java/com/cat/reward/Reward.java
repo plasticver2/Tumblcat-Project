30adb707a2b2f6c11fc08bc06e92f277e7ff20e1
plasticver2 <108119032+plasticver2@users.noreply.github.com>
2022-09-12 02:47:36 +0900

    탈퇴 기능 구현 최종 ( java 폴더, resources 폴더)

---
+++ java/com/cat/reward/Reward.java
@@ -0,0 +1,40 @@
+package com.cat.reward;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.cat.project.entity.Project;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Reward {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long rwId;
+	
+	@ManyToOne
+	private Project project;
+	
+	@Column(nullable = false)
+	private BigDecimal rwMin;
+	
+	@Column(nullable = false)
+	private BigDecimal rwMax;
+	
+	@Column(nullable = false, length = 64)
+	private String rwName;
+	
+	@Column(nullable = false, columnDefinition = "TEXT") 
+	private String rwDesc;
+}
